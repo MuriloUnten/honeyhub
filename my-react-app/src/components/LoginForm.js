6dc14d1e107420a41d49016c16ec6834143dae9d
Thomas <thomaspinton@alunos.utfpr.edu.br>
2024-05-11 23:52:15 -0300

    sign up screen created (only front end, no code)

---
+++ my-react-app/src/components/LoginForm.js
@@ -0,0 +1,33 @@
+const LoginForm = () => {
+    return (
+        <div className="m-auto">
+            <form>
+                <h1 className='text-white font-bold text-6xl mx-auto mb-14'>Join the hive!</h1>
+
+                <div className="mb-8">
+                    <div className="text-white mb-1 ml-2 text-xl">Email</div>
+                    <input type="text" placeholder="Enter your email" className="w-full h-11 rounded-3xl p-5"></input>
+                </div>
+                <div className="mb-8">
+                    <div className="text-white mb-1 ml-2 text-xl">Username</div>
+                    <input type="text" placeholder="Enter your username" className="w-full h-11 rounded-3xl p-5"></input>
+                </div>
+                <div className="mb-10">
+                    <div className="text-white mb-1 ml-2 text-xl">Password</div>
+                    <input type="password" placeholder="Enter your password" className="w-full h-11 rounded-3xl p-5"></input>
+                </div>
+                <div className="">
+                    <button type="submit" className="bg-main text-lg w-full h-11 rounded-3xl mb-2">Sign Up</button>
+                    <div className="flex">
+                        <div className="flex text-white m-auto">
+                            <div className="w-auto mr-1">Already have an account?</div><div className="text-main mr-2">Sign up</div>
+                        </div>
+                    </div>
+                    </div>
+            </form>
+        </div>
+    )
+}
+
+//<div className="bg-black2 text-black3 h-11 flex items-center rounded-xl">{placeHolder}</div>
+export default LoginForm;
