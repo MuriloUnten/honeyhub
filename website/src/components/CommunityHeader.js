8913820b163f8fa0cb5a0930747ca61076c95f47
Murilo Unten <murilo.unten@proton.me>
2024-05-13 21:34:14 -0300

    style community header

--- website/src/components/CommunityHeader.js
+++ website/src/components/CommunityHeader.js
@@ -1,13 +1,22 @@
+import banner from "../assets/utfpr-placeholder.png"
+import icon from "../assets/utfpr-icon-placeholder.jpg"
 
 const CommunityHeader = () => {
     return (
-        <div className='w-full h-48 bg-black2 rounded-3xl mb-4 flex mx-auto'>
-            <div>
-                Add Community Banner here
+        <div className='w-full h-92 bg-black2 rounded-3xl mb-4 flex flex-col mx-auto'>
+            <div className='w-full h-32 px-4 pt-4'>
+                <div className='w-full h-full'>
+                    <img src={banner} className='h-full w-full rounded-2xl overflow-hidden'></img>
+                </div>
             </div> 
-            <div>
-                <div>Community Icon here</div>
-                <div>Community name</div>
+            <div className='w-full flex px-8 py-3 justify-between items-center'>
+                <div className='h-14 flex justify-between items-center'>
+                    <img src={icon} className='h-full rounded-xl '></img>
+                    <div className='text-3xl font-semibold align-text-top pl-5 items-center'>h/utfpr</div>
+                </div>
+                <div className='bg-main rounded-3xl h-10 w-24 flex items-center text-center justify-end'>
+                    <button className='text-lg mx-auto' >Follow</button>
+                </div>
             </div>
         </div> 
     );
