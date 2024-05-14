0694e17482226a4f8f75d7eccedcbbb4e74c32cc
vitkenji <vkz.yamada@gmail.com>
2024-05-14 10:02:57 -0300

    correction

--- website/src/screens/LogIn.js
+++ website/src/screens/LogIn.js
@@ -1,30 +1,30 @@
-import beeHive from '../assets/beeHiveLogIn.png' 
-import LogInForm from '../components/LogInForm';
-import { redirect } from "react-router-dom"
+import beeHiveLogIn from "../assets/beeHiveLogIn.png";
+import LogInForm from "../components/LogInForm";
+import { redirect } from "react-router-dom";
 
 const LogIn = () => {
-    return (
-        <div className='flex h-screen'>
-            <div className='w-7/12 bg-black1 h-full flex'>
-                <LogInForm />
-            </div>
-            <img src={beeHive} className='w-5/12 h-full'></img>
-        </div>
-    )
-}
+  return (
+    <div className="flex h-screen">
+      <div className="w-7/12 bg-black1 h-full flex">
+        <LogInForm />
+      </div>
+      <img src={beeHiveLogIn} className="w-5/12 h-full"></img>
+    </div>
+  );
+};
 
 export default LogIn;
 
 export const logInAction = async ({ request }) => {
-    const data = await request.formData()   
-    const submission = {
-        email: data.get("email"),
-        password: data.get("password") // TODO Fix me (terrible idea)
-    }
-    console.log(submission) // TODO Remove me (must call backend here)
+  const data = await request.formData();
+  const submission = {
+    email: data.get("email"),
+    password: data.get("password"), // TODO Fix me (terrible idea)
+  };
+  console.log(submission); // TODO Remove me (must call backend here)
 
-    // send submission to backend to authenticate user
-    // if authenticated then redirect to feed
-    // else show error message
-    return redirect("/feed")
-}
+  // send submission to backend to authenticate user
+  // if authenticated then redirect to feed
+  // else show error message
+  return redirect("/feed");
+};
