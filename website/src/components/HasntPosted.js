639fbf4bc334e1c61da474700089ae1608f6574b
vitkenji <vkz.yamada@gmail.com>
2024-05-17 08:46:50 -0300

    view profile redirecting

---
+++ website/src/components/HasntPosted.js
@@ -0,0 +1,11 @@
+const HasntPosted = () => {
+  return (
+    <div className="w-3/12 h-96 bg-black2 rounded-3xl">
+      <div className="text-white text-lg text-center mx-auto">
+        User hasn't posted yet!
+      </div>
+    </div>
+  );
+};
+
+export default HasntPosted;
--- website/src/components/ViewProfile.js
+++ website/src/components/ViewProfile.js
@@ -1,32 +1,74 @@
+import React, { useState } from "react";
+import Comments from "./Comments";
+import { PostsSection } from "./PostsSection";
 import profile from "../assets/profile.png";
+import HasntPosted from "./HasntPosted";
 
 const ViewProfile = () => {
+  const [activeComponent, setActiveComponent] = useState("Overview");
+
+  const renderComponent = () => {
+    switch (activeComponent) {
+      case "Overview":
+        return <PostsSection />;
+      case "Posts":
+        return <HasntPosted />;
+      case "Comments":
+        return <Comments />;
+      case "Upvoted":
+        return <PostsSection />;
+      case "Downvoted":
+        return <PostsSection />;
+      default:
+        return <PostsSection />;
+    }
+  };
+
   return (
-    <div className="w-full h-80 bg-black2 rounded-3xl px-8 py-2 mb-4">
+    <div className="w-full bg-black2 rounded-3xl px-8 py-2 mb-4">
       <div className="h-40">
-        <img src={profile} className="max-h-40"></img>
+        <img src={profile} className="max-h-40" alt="Profile" />
       </div>
       <div className="ml-4">
         <div className="font-bold text-3xl text-white mb-1">username</div>
       </div>
       <div className="mt-10">
         <ul className="flex justify-between space-x-4">
-          <li className="font-bold text-2xl text-white hover:underline cursor-pointer hover:text-main">
+          <li
+            className="font-bold text-2xl text-white hover:underline cursor-pointer hover:text-main"
+            onClick={() => setActiveComponent("Overview")}
+          >
             Overview
           </li>
-          <li className="font-bold text-2xl text-white hover:underline cursor-pointer hover:text-main">
+          <li
+            className="font-bold text-2xl text-white hover:underline cursor-pointer hover:text-main"
+            onClick={() => setActiveComponent("Posts")}
+          >
             Posts
           </li>
-          <li className="font-bold text-2xl text-white hover:underline cursor-pointer hover:text-main">
+          <li
+            className="font-bold text-2xl text-white hover:underline cursor-pointer hover:text-main"
+            onClick={() => setActiveComponent("Comments")}
+          >
             Comments
           </li>
-          <li className="font-bold text-2xl text-white hover:underline cursor-pointer hover:text-main">
+          <li
+            className="font-bold text-2xl text-white hover:underline cursor-pointer hover:text-main"
+            onClick={() => setActiveComponent("Upvoted")}
+          >
             Upvoted
           </li>
-          <li className="font-bold text-2xl text-white hover:underline cursor-pointer hover:text-main">
+          <li
+            className="font-bold text-2xl text-white hover:underline cursor-pointer hover:text-main"
+            onClick={() => setActiveComponent("Downvoted")}
+          >
             Downvoted
           </li>
         </ul>
+      </div>
+      <div className="mt-8 mb-4 border-b border-white"></div>
+      <div className="w-full bg-black2 rounded-3xl px-8 py-2 mb-4">
+        {renderComponent()}
       </div>
     </div>
   );
