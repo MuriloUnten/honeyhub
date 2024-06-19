b1d144564ccf5ab751254b811bcc1fdf234bbe7d
Thomas <thomaspinton@alunos.utfpr.edu.br>
2024-06-19 01:34:50 -0300

    comments now load from backend (not hardcoded)

--- website/src/components/Comments.js
+++ website/src/components/Comments.js
@@ -2,17 +2,18 @@ import leftArrow from "../assets/leftArrow.png";
 import { Link } from "react-router-dom";
 import profile from "../assets/profile.png";
 import beach from '../assets/beach.jpg';
-import comment from '../assets/comment.png';
+import commentImg from '../assets/comment.png';
 import share from '../assets/share.png';
 import upArrow from '../assets/upArrow.png';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-const Comments = () => {
+const Comments = ({comment}) => {
     const [upVotes, setUpVotes] = useState(0);
     const [upVoted, setUpVoted] = useState(false);
     const [downVoted, setDownVoted] = useState(false);
     const [originalState, setOriginal] = useState(true);
-    const [comment, setComment] = useState([])
+
+    const { post, user, community } = comment;
 
     const incrementVotes = () => {
         if (originalState) {
@@ -46,26 +47,26 @@ const Comments = () => {
         }
     };
 
-    const fetchCommentData = async () => {
-      try {
-        const response = await fetch(BASE_URL + '/post/' + id + 'comments')
-        if (!response.ok) {
-            throw new Error('Failed to fetch post data');
-        }
-        const postData = await response.json();
-      } catch (error) {
-          console.error('Error fetching post:', error);
-      }
-    }
+    // const fetchCommentData = async () => {
+    //   try {
+    //     const response = await fetch(BASE_URL + '/post/' + id + 'comments')
+    //     if (!response.ok) {
+    //         throw new Error('Failed to fetch post data');
+    //     }
+    //     const postData = await response.json();
+    //   } catch (error) {
+    //       console.error('Error fetching post:', error);
+    //   }
+    // }
 
     return (
         <div className="w-full bg-black2 rounded-3xl px-8 py-2 mb-4">
             <div className="mt-2 h-16 flex items-center">
                 <Link to="/profile"><img src={profile} className="max-h-10 ml-2" alt="Profile"></img></Link>
-                <Link to="/profile"><div className="text-white font-bold text-xl ml-4">username2</div></Link>
+                <Link to="/profile"><div className="text-white font-bold text-xl ml-4">{user.username}</div></Link>
             </div>
-            <div className="text-xl text-white h-10 mb-1 mt-2 ml-8">
-                This is a comment
+            <div className="text-white h-10 mb-1 mt-2 ml-8">
+                { post.body } 
             </div>
             <div className='flex justify-between mb-1'>
                 <div className='ml-auto flex items-center'>
@@ -75,7 +76,7 @@ const Comments = () => {
                         <img src={upArrow} className='w-5' alt="Downvote" onClick={decreaseVotes}></img>
                     </div>  
                     <div className='w-12 bg-black3 flex items-center justify-around px-2 py-2 rounded-xl'>
-                        <img src={comment} className='w-6 mx-1' alt="Comment"></img>
+                        <img src={commentImg} className='w-6 mx-1' alt="Comment"></img>
                     </div>
                 </div>
             </div>
--- website/src/screens/ViewPost.js
+++ website/src/screens/ViewPost.js
@@ -3,19 +3,46 @@ import RightBar from "../components/RightBar";
 import Comments from "../components/Comments";
 import VisualizePost from "../components/VisualizePost";
 import CreateCommentForm from "../components/CreateCommentForm";
+import { useState, useEffect } from "react";
+import {useParams} from "react-router-dom"
+import { BASE_URL } from "../backend_url";
 
 const ViewPost = () => {
+
+  const [comments, setComments] = useState([]);
+    const {id} = useParams()
+
+  useEffect(() => {
+    fetch(BASE_URL + '/post/' + id + '/comments') // Replace '/api/comments' with the actual endpoint
+      .then(response => response.json())
+      .then(data => setComments(data))
+      .catch(error => console.error('Error fetching comments:', error));
+  }, []);
+
   return (
     <div className="bg-black1 w-h-full flex p-6 justify-around">
       <LeftBar></LeftBar>
       <div className="w-6/12 rounded-3xl">
         <VisualizePost></VisualizePost>
         <CreateCommentForm></CreateCommentForm>
-        <Comments></Comments>
+        <CommentList comments={comments}></CommentList>
       </div>
       <RightBar></RightBar>
     </div>
   );
 };
+
+const CommentList = ({ comments }) => {
+  return (
+    <div>
+      {comments.length > 0 ? (
+        comments.map((comment) => <Comments comment={comment} />)
+      ) : (
+        <div className="text-white">No comments yet.</div>
+      )}
+    </div>
+  );
+};
+
 
 export default ViewPost;
