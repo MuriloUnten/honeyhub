1f9a293bbcb29bed0bdf02f75695fbd303bc677e
Thomas <thomaspinton@alunos.utfpr.edu.br>
2024-06-19 02:44:57 -0300

    removed share icon from post

--- website/src/components/Post.js
+++ website/src/components/Post.js
@@ -43,12 +43,9 @@ const Post = (props) => {
                     <img src={upArrow} className='w-5'></img>
                 </div>
                 <div className='flex justify-end'>
-                    <div className='w-12 bg-black3 flex items- justify-around px-2 py-2 rounded-xl mr-4'>
+                    <div className='w-12 bg-black3 flex items- justify-around px-2 py-2 rounded-xl'>
                         <img src={comment} className='w-6 mx-1'></img>
                     </div>   
-                    <div className='w-10 bg-black3 flex items-center px-2 py-2 rounded-xl'>
-                        <img src={share} className='w-6 mr-2'></img>
-                    </div>    
                 </div>
             </div>
         </div>
